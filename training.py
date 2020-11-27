83bb17d04e5a85c7ece374bc5b46d12cf112f5b1
Marcos Galvão <matg@cin.ufpe.br>
2020-11-27 14:06:04 -0300

    Update training.py

--- training.py
+++ training.py
@@ -79,8 +79,7 @@ class OthelloMCTS(MCTS):
         network_probabilities = self.get_state_actions_propabilities(state)
         mask = self._mask_valid_moves(state)
         probabilities = network_probabilities * mask
-        probabilities_scaled /= np.sum(probabilities)
-        return probabilities_scaled
+        return probabilities
     
     def _mask_valid_moves(self, state):
         board_mask = np.zeros((self._board_size, self._board_size))

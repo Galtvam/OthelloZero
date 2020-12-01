852bdce6ff0b101990de77d22647f3f36692662c
Marcos Galvão <matg@cin.ufpe.br>
2020-11-30 21:10:36 -0300

    Adjust in network architecture

--- Net/OthelloNN.py
+++ Net/OthelloNN.py
@@ -35,31 +35,22 @@ class OthelloNN:
     self.action_size = self.board_x * self.board_y
     self.learning_rate = lr
     self.dropout = dropout
-    self.num_channels_1 = num_channels_1
-    self.num_channels_2 = num_channels_2
+    self.num_channels = num_channels_1
+    #self.num_channels_2 = num_channels_2
 
 
     self.input_boards = Input(shape=(self.board_x, self.board_y, 2)) #shape (batch_size, board_x, board_y, 2)
-
-    conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels_1, 3, padding='same', use_bias=False)(self.input_boards))) #shape (batch_size, board_x, board_y, num_channels_1)
-    conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels_1, 3, padding='same', use_bias=False)(conv1))) #shape (batch_size, board_x, board_y, num_channels_1)
-    special1 = MaxPooling2D((2, 2), strides=(2, 2), padding='valid')(conv2) #shape (batch_size, board_x/2, board_y/2, num_channels_1)
-
-    conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels_2, 3, padding='same', use_bias=False)(special1))) #shape (batch_size, board_x/2, board_y/2, num_channels_2)
-    conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels_2, 3, padding='same', use_bias=False)(conv3))) #shape (batch_size, board_x/2, board_y/2, num_channels_2)
-
-    flatten = Flatten()(conv4)  #shape (batch_size, board_x/2 x board_y/2 x num_channels_2)
-
-    #value side
-    v_dense1 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(flatten)))) # shape (batch_size x 512)
-    v_dense2 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(256, use_bias=False)(v_dense1)))) # shpe (batch_size x 256)
-    self.v = Dense(1, activation='tanh', name='v')(v_dense2) # shape (batch_size x 1)
-
-    #pi side
-    pi_dense1 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(flatten)))) # shape (batch_size x 512)
-    pi_dense2 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(256, use_bias=False)(pi_dense1)))) # shape (batch_size x 256)
-    self.pi = Dense(self.action_size, activation='softmax', name='pi')(pi_dense2) # shape (batch_size x action_size)
-    self.pi = Reshape((self.board_x, self.board_y))(self.pi)
+    h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='same')(self.input_boards)))         
+    h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='same')(h_conv1)))         
+    h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='valid')(h_conv2)))        
+    h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='valid')(h_conv3)))       
+    h_conv4_flat = Flatten()(h_conv4)       
+    s_fc1 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024)(h_conv4_flat)))) 
+    s_fc2 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512)(s_fc1))))          
+    pi = Dense(self.action_size, activation='softmax')(s_fc2)
+    self.pi = Reshape((self.board_x, self.board_y), name='pi')(pi)  
+    self.v = Dense(1, activation='tanh', name='v')(s_fc2)
 
     self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
-    self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(self.learning_rate, clipvalue=0.5))
+    self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], 
+                       optimizer=Adam(self.learning_rate, clipvalue=0.5))
